ee7f7c41cf70a366b29afafb3bacf7b89262de3d
kimdabin <suor1058@naver.com>
2022-12-31 16:37:47 +0900

    변경

--- works/player/js/function.js
+++ works/player/js/function.js
@@ -1,18 +1,20 @@
 const $myvideo = document.getElementById('myvideo');
 
-const $btnPlay = $('.play');
-const $btnPause = $('.pause');
-const $btnResume = $('.resume');
+const $btnPlay= $('.play');
+const $btnPause= $('.pause');
+const $btnResume= $('.resume');
 
-$btnPlay.on('click',  function(){
-    $myvideo.load();
+$btnPlay.on('click', function(){
+    $myvideo.load(); //처음으로 다시 되돌아감. 즉 처음부터 시작.
+    $myvideo.play(); //멈춘 지점에서 재생함.
+});
+
+$btnPause.on('click',function(){
+    $myvideo.pause(); //일시정지
+});
+
+$btnResume.on('click',function(){
     $myvideo.play();
 });
 
-$btnPause.on('click', function(){
-    $myvideo.pause();
-});
 
-$btnResume.on('click', function(){
-    $myvideo.play();
-});
